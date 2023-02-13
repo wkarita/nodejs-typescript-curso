efd46e00ae66c187bfc23cb2bd3acb782d03c79f
Wilson Katudi Arita Junior <wilson@drcuco.com.br>
2023-02-13 14:08:14 -0300

    feat: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("teste")
+import * as express from "express"
+import { config } from "dotenv"
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
