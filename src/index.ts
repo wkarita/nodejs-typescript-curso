1f1de2257759f2478d7d6b4a034cfe280582da1d
Wilson Katudi Arita Junior <wilson@drcuco.com.br>
2023-02-13 16:22:23 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,25 @@ import { GetUsersController } from "./controllers/get-users/get-users"
 import * as express from "express"
 import { config } from "dotenv"
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users"
+import { MongoClient } from "./database/mongo"
 
-config()
+const main = async () => {
+  config()
 
-const app = express()
+  const app = express()
 
-const port = process.env.PORT || 8000
+  await MongoClient.connect()
 
-app.get("/users", async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository()
-  const getUsersController = new GetUsersController(mongoGetUsersRepository)
-  const { body, statusCode } = await getUsersController.handle()
-  res.status(statusCode).send(body)
-})
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository()
+    const getUsersController = new GetUsersController(mongoGetUsersRepository)
+    const { body, statusCode } = await getUsersController.handle()
+    res.status(statusCode).send(body)
+  })
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+  const port = process.env.PORT || 8000
+
+  app.listen(port, () => console.log(`Listening on port ${port}!`))
+}
+
+main()
