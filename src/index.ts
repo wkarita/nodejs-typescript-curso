3316008dee4579881185db57ed506a39602db307
Wilson Katudi Arita Junior <wilson@drcuco.com.br>
2023-02-13 14:37:45 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,8 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
+app.get("/", (req, res) => {
+  res.send("Hello Word!")
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`))
