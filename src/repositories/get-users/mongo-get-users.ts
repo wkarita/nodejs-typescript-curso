9bf0d243cfd4bcce5a9c58baa0bece733dd969f1
Wilson Katudi Arita Junior <wilson@drcuco.com.br>
2023-02-13 16:34:19 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,14 @@
 import { IGetUsersRepository } from "@/controllers/get-users/protocols"
+import { MongoClient } from "@/database/mongo"
 import { User } from "@/models/user"
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Felipe",
-        lastName: "Rocha",
-        email: "teste@teste.com",
-        password: "test",
-      },
-    ]
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray()
+
+    return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }))
   }
 }
