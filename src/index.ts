8c24341fd63d9e0ecdda37f37830d348f15c4963
Wilson Katudi Arita Junior <wilson@drcuco.com.br>
2023-02-14 15:52:14 -0300

    feat: add DeleteUserController and its route

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-user
 import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user"
 import { MongoUpdateUserRepository } from "./repositories/update-user/mongo-update-user"
 import { MongoClient } from "./database/mongo"
+import { MongoDeleteUserRepository } from "./repositories/delete-user/mongo-delete-user"
+import { DeleteUserController } from "./controllers/delete-user/delete-user"
 
 const main = async () => {
   config()
@@ -42,6 +44,17 @@ const main = async () => {
     )
     const { body, statusCode } = await updateUserController.handle({
       body: req.body,
+      params: req.params,
+    })
+    res.status(statusCode).send(body)
+  })
+
+  app.delete("/users/:id", async (req, res) => {
+    const mongoDeleteUserRepository = new MongoDeleteUserRepository()
+    const deleteUserController = new DeleteUserController(
+      mongoDeleteUserRepository
+    )
+    const { body, statusCode } = await deleteUserController.handle({
       params: req.params,
     })
     res.status(statusCode).send(body)
